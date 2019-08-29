ee0e534a2c769c5ee62453f2689777978b5333bd
nikhil parlapalli <nikhilp.bits@gmail.com>
2019-08-29 15:38:10 -0400

    Resolved issue on SolidYellowCurve image by changing th ehofman threshold

--- P1.py
+++ P1.py
@@ -6,11 +6,11 @@ import cv2
 #get_ipython().run_line_magic('matplotlib', 'inline')
 
 #reading in an image
-image = mpimg.imread('test_images/solidWhiteRight.jpg')
+#image = mpimg.imread('test_images/solidYellowCurve2.jpg')
 
 #printing out some stats and plotting
-print('This image is:', type(image), 'with dimensions:', image.shape)
-plt.imshow(image)  # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
+#print('This image is:', type(image), 'with dimensions:', image.shape)
+#plt.imshow(image)  # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
 
 import math
 
@@ -86,6 +86,7 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     Returns an image with hough lines drawn.
     """
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    print('lines', lines)
     line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
     draw_lines(line_img, lines)
     return line_img
@@ -114,12 +115,16 @@ def process_image(image):
 # then save them to the test_images_output directory.
 
     gray = grayscale(image)
- 
     kernel_size = 3
     blur_gray = gaussian_blur(gray, kernel_size)
+    #plt.imshow(blur_gray)
+    ##plt.show()
+    
     low_threshold = 100
     high_threshold = 150
     canny_out = canny(blur_gray, low_threshold, high_threshold)
+    plt.imshow(canny_out,cmap='gray')
+    #plt.show()
  
     # give the vertices of polygon in an array form
     ysize = image.shape[0]
@@ -127,22 +132,31 @@ def process_image(image):
     vertices = np.array([[[100, ysize], [450,325],[525,325], [850,ysize]]], dtype=np.int32)
     #Region of interest
     masked_image = region_of_interest(canny_out, vertices)
+    plt.imshow(masked_image,cmap='gray')
+    #plt.show()
  
     #Hough transforms
     img = masked_image
     rho = 1
     theta = np.pi/180
     threshold = 10
-    min_line_len = 10
+    min_line_len = 15
     max_line_gap = 2
     line_img = hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap)
- 
+    plt.imshow(line_img)
+    #plt.show()
+    
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]),min_line_len, max_line_gap)
-
     pos_slope_line_x = []
     pos_slope_line_y = []
     neg_slope_line_x = []
     neg_slope_line_y = []
+    fit_left = []
+    fit_right = []
+    xpos_bottomleft = 0
+    xpos_topleft = 0
+    xpos_bottomrigt = 0
+    xpos_topright = 0
     
     for line in lines:
         for x1,y1,x2,y2 in line:
@@ -169,128 +183,44 @@ def process_image(image):
     xpos_topright = (325 - fit_right[1])/fit_right[0]
     cv2.line(line_img, (int(xpos_bottomleft), ysize), (int(xpos_topleft), 325), 255, 20)
     cv2.line(line_img, (int(xpos_bottomrigt), ysize), (int(xpos_topright), 325), 255, 20)
-    if(int(xpos_bottomrigt) > xsize):
-        print('bot right = ', xpos_bottomrigt)
-        print('pos slope = ', fit_left[0])
-    if(int(xpos_topright) < 0):
-        print('topright = ', xpos_topright)
-        print('neg slope = ', fit_right[0])
-        
-    #draw_lines(line_img, lines, color=[200, 0, 0], thickness=25)
- 
+    
     weighted_image = weighted_img(line_img, image, α=1, β=0.5, γ=0.)
- 
+    
+    #debug information
+    print('positive line x cords',sorted(pos_slope_line_x))
+    print('positive line y cords',sorted(pos_slope_line_y))
+    print('slope , intercept ' , fit_left[0] , fit_left[1])
+    print('xpos_bottomleft , xpos_topleft ' , xpos_bottomleft , xpos_topleft)
+    
+    plt.imshow(weighted_image)
+    #plt.show()
+
     result = weighted_image
-
-
     return result
     
 
 import os
 images = os.listdir("test_images/")
 img_save = 'test_images_output/'
-#image = mpimg.imread('test.jpg')
 
-# TODO: Build your pipeline that will draw lane lines on the test_images
-# then save them to the test_images_output directory.
+#TODO: Build your pipeline that will draw lane lines on the test_images
+#then save them to the test_images_output directory.
 for image in images:
-    print('image = ', "test_images/" + image)
-    print('This image is:', type(mpimg.imread("test_images/" + image)), 'with dimensions:', mpimg.imread("test_images/" + image).shape)
+#    print(image)
     output_image = process_image(mpimg.imread("test_images/" + image))
-    #plt.imshow(output_image)
-    #plt.show()
-    mpimg.imsave("test_images_output/" + image, output_image)
-
-
+    mpimg.imsave(img_save + image, output_image)
+#process_image(image)
+    
 # Import everything needed to edit/save/watch video clips
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
-
-def process_image(image):
-    # NOTE: The output you return should be a color image (3 channel) for processing video below
-    # TODO: put your pipeline here,
-    # you should return the final output (image where lines are drawn on lanes)
-    # TODO: Build your pipeline that will draw lane lines on the test_images
-# then save them to the test_images_output directory.
-
-    gray = grayscale(image)
- 
-    kernel_size = 3
-    blur_gray = gaussian_blur(gray, kernel_size)
-    low_threshold = 100
-    high_threshold = 150
-    canny_out = canny(blur_gray, low_threshold, high_threshold)
- 
-    # give the vertices of polygon in an array form
-    ysize = image.shape[0]
-    xsize = image.shape[1]
-    vertices = np.array([[[100, ysize], [450,325],[525,325], [850,ysize]]], dtype=np.int32)
-    #Region of interest
-    masked_image = region_of_interest(canny_out, vertices)
- 
-    #Hough transforms
-    img = masked_image
-    rho = 1
-    theta = np.pi/180
-    threshold = 10
-    min_line_len = 10
-    max_line_gap = 2
-    line_img = hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap)
- 
-    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]),min_line_len, max_line_gap)
-
-    pos_slope_line_x = []
-    pos_slope_line_y = []
-    neg_slope_line_x = []
-    neg_slope_line_y = []
     
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            if((y2-y1)/(x2-x1) > 0):
-                pos_slope_line_x.append(x1)
-                pos_slope_line_x.append(x2)
-                pos_slope_line_y.append(y1)
-                pos_slope_line_y.append(y2)
-            else:
-                neg_slope_line_x.append(x1)
-                neg_slope_line_x.append(x2)
-                neg_slope_line_y.append(y1)
-                neg_slope_line_y.append(y2)
-                
-    fit_left =  np.polyfit(pos_slope_line_x,pos_slope_line_y,1)
-    fit_right = np.polyfit(neg_slope_line_x,neg_slope_line_y,1)
-    
-    # y =fit_left[0] * x + fit_left[1]
-    # ysize = fit_left[0] *x + fit_left[1]
-    # x = ysize - fit_left[1] dividedby fit_left[0]
-    xpos_bottomleft = (ysize - fit_left[1])/fit_left[0]
-    xpos_topleft = (325 - fit_left[1])/fit_left[0]
-    xpos_bottomrigt = (ysize - fit_right[1])/fit_right[0]
-    xpos_topright = (325 - fit_right[1])/fit_right[0]
-    cv2.line(line_img, (int(xpos_bottomleft), ysize), (int(xpos_topleft), 325), 255, 20)
-    cv2.line(line_img, (int(xpos_bottomrigt), ysize), (int(xpos_topright), 325), 255, 20)
-    if(int(xpos_bottomrigt) > xsize):
-        print('bot right = ', xpos_bottomrigt)
-        print('pos slope = ', fit_left[0])
-    if(int(xpos_topright) < 0):
-        print('topright = ', xpos_topright)
-        print('neg slope = ', fit_right[0])
-        
-    #draw_lines(line_img, lines, color=[200, 0, 0], thickness=25)
- 
-    weighted_image = weighted_img(line_img, image, α=1, β=0.5, γ=0.)
- 
-    result = weighted_image
-
-
-    return result
-    
-white_output = 'test_videos_output/solidWhiteRight.mp4'
-## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
-## To do so add .subclip(start_second,end_second) to the end of the line below
-## Where start_second and end_second are integer values representing the start and end of the subclip
-## You may also uncomment the following line for a subclip of the first 5 seconds
-##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
-clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,10)
-white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
-white_clip.write_videofile(white_output, audio=False)
+# white_output = 'test_videos_output/solidWhiteRight.mp4'
+# ## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+# ## To do so add .subclip(start_second,end_second) to the end of the line below
+# ## Where start_second and end_second are integer values representing the start and end of the subclip
+# ## You may also uncomment the following line for a subclip of the first 5 seconds
+# ##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
+# clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,10)
+# white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+# white_clip.write_videofile(white_output, audio=False)
